3b2213241f079e47c1c8f08425fcc0ecb7e6bfad
ErikMj12 <erik12.ixec@gmail.com>
2017-05-03 16:50:31 -0500

    App.js modified

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = require('http');
 
 app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === <VERIFY_TOKEN>) {
+      req.query['hub.verify_token'] === '836812403e08c9f06cfb841352428fbb') {
     console.log("Validating webhook");
     res.status(200).send(req.query['hub.challenge']);
   } else {
